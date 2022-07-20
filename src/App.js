fb045d5abb6f9ec8f65d2ae2d96c5da3e8966902
Alaina H <97alaina@gmail.com>
2022-07-19 22:12:34 -0400

    some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
+import Navbar from './Components/Navbar';
 import './App.css';
+
+// The website must have the following pages: 
+// Home Page (Static): Working slider (does not have to be the hero, can be anywhere on the home page)
+// Products Page (React): 12 products with a working filter rendered from your Database. Each product should have a title, description, price, and image that is rendered from your Database
+// Working filter for price and product type
+// Contact Page (Static): JavaScript form validation (or any kind of functional validation for email)
+// The form should at least include a place for name, email, and comment. 
+// Responsiveness
+// Both tablet and mobile
+// Each page should have a Navigation Bar and Footer with links to social media (these should be components rendered, not static).
+
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-       
-      </header>
+       <Navbar />
+      
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+//rfce vs frc
+import React from 'react';
+import '../App.css';
+function navbar() {
+  return (
+    <div className='Navbar'>
+        <div className='leftSide'>
+            <div className='links'>
+                <a href="#">Home</a>
+                <a href="#">Shop</a>
+                <a href="#">Contact Us</a>
+                <a href="#">?</a>
+            </div>
+        </div>
+        <div className='rightSide'></div>
+    </div>
+  )
+}
+
+export default navbar
