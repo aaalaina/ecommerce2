67c38247cdca6b60f8b7f56fe7cc0b5ed4384880
Alaina H <97alaina@gmail.com>
2022-07-26 23:51:04 -0400

    Changed a few things around

--- src/Components/Products.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Products {
-  render() {
-    return (
-      <div>In progress....</div>
-    )
-  }
-}
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,2 +1,14 @@
-import React from "react";
 
+import React from 'react';
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import DisplayCard from '../Components/DisplayCard'
+export default function Products() 
+    const getProducts = () => {
+   Axios.get('https://fuwafuwapets.herokuapp.com/ProductList')
+   .then(response => {
+    console.log(response)
+   }).catch(error => {
+    console.log(error)
+   })
+   }
